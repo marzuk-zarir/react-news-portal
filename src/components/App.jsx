3411fd4aa14582eb7ed2b22acb023d0e9a453ee3
marzuk-zarir <marzukzarir@gmail.com>
2022-06-05 12:25:07 +0600

    [feat]: Intialize a brand new repo

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import { Button } from '@mui/material'
+
+export default function App() {
+    return (
+        <div>
+            <Button variant="contained">mui button</Button>
+        </div>
+    )
+}
